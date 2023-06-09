8c8423fba342135d1ce715aba86aa872aeaa32af
ariandto <92358210+ariandto@users.noreply.github.com>
2023-06-10 06:55:47 +0700

    commot all bro

--- server/server.js
+++ server/server.js
@@ -1,170 +1,185 @@
-import express from 'express';
-import mysql from 'mysql';
-import cors from 'cors';
-import bcrypt from 'bcrypt';
+  import express from 'express';
+  import mysql from 'mysql';
+  import cors from 'cors';
+  import bcrypt from 'bcrypt';
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-const port = 8081;
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+  const port = 8081;
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'klsroute',
-});
+  const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'klsroute',
+  });
 
-db.connect((err) => {
-  if (err) {
-    console.error('Error connecting to database: ', err);
-  } else {
-    console.log('Connected to database');
-  }
-});
-
-// Rute untuk form login
-app.get('/login', (req, res) => {
-  res.sendFile(__dirname + '/login.html');
-});
-
-app.get('/', (req, res) => {
-  const sql = 'SELECT * FROM route';
-  db.query(sql, (err, result) => {
+  db.connect((err) => {
     if (err) {
-      console.error('Error executing SQL query: ', err);
-      res.status(500).json({ message: 'Error inside server' });
+      console.error('Error connecting to database: ', err);
     } else {
-      res.json(result);
+      console.log('Connected to database');
     }
   });
-});
 
-app.post('/route', (req, res) => {
-  const { shiptoname, address, remarks, area } = req.body;
+  // Rute untuk form login
+  app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/login.html');
+  });
 
-  // Get the last inserted ID from the table
-  const sqlLastId = 'SELECT id FROM route ORDER BY id DESC LIMIT 1';
-  db.query(sqlLastId, (err, result) => {
-    if (err) {
-      console.error('Error executing SQL query: ', err);
-      res.status(500).json(err);
-    } else {
-      let lastId = 0;
-      if (result.length > 0) {
-        lastId = parseInt(result[0].id.slice(3), 10); // Extract the numeric part of the last ID
+  app.get('/', (req, res) => {
+    const sql = 'SELECT * FROM route';
+    db.query(sql, (err, result) => {
+      if (err) {
+        console.error('Error executing SQL query: ', err);
+        res.status(500).json({ message: 'Error inside server' });
+      } else {
+        res.json(result);
       }
+    });
+  });
 
-      const newId = 'KLS' + String(lastId + 1).padStart(3, '0'); // Create the new ID
+  app.post('/route', (req, res) => {
+    const { shiptoname, address, remarks, area } = req.body;
 
-      const sql = 'INSERT INTO route (id, shiptoname, address, remarks, area) VALUES (?, ?, ?, ?, ?)';
-      const values = [newId, shiptoname, address, remarks, area];
-
-      db.query(sql, values, (err, result) => {
-        if (err) {
-          console.error('Error executing SQL query: ', err);
-          res.status(500).json(err);
-        } else {
-          const insertedData = { id: newId, shiptoname, address, remarks, area };
-          res.json(insertedData);
+    // Get the last inserted ID from the table
+    const sqlLastId = 'SELECT id FROM route ORDER BY id DESC LIMIT 1';
+    db.query(sqlLastId, (err, result) => {
+      if (err) {
+        console.error('Error executing SQL query: ', err);
+        res.status(500).json(err);
+      } else {
+        let lastId = 0;
+        if (result.length > 0) {
+          lastId = parseInt(result[0].id.slice(3), 10); // Extract the numeric part of the last ID
         }
-      });
-    }
+
+        const newId = 'KLS' + String(lastId + 1).padStart(3, '0'); // Create the new ID
+
+        const sql = 'INSERT INTO route (id, shiptoname, address, remarks, area) VALUES (?, ?, ?, ?, ?)';
+        const values = [newId, shiptoname, address, remarks, area];
+
+        db.query(sql, values, (err, result) => {
+          if (err) {
+            console.error('Error executing SQL query: ', err);
+            res.status(500).json(err);
+          } else {
+            const insertedData = { id: newId, shiptoname, address, remarks, area };
+            res.json(insertedData);
+          }
+        });
+      }
+    });
   });
-});
 
-app.post('/register', (req, res) => {
-  const { userid, username, password } = req.body;
+  app.post('/register', (req, res) => {
+    const { userid, username, password } = req.body;
 
-  // Periksa apakah pengguna sudah terdaftar
-  const checkUserQuery = 'SELECT * FROM user WHERE username = ?';
-  db.query(checkUserQuery, [username], (err, result) => {
-    if (err) {
-      console.error('Error executing SQL query: ', err);
-      res.status(500).json({ error: 'Terjadi kesalahan saat mendaftar.' });
-    } else if (result.length > 0) {
-      res.status(409).json({ error: 'Username sudah digunakan.' });
-    } else {
-      // Enkripsi password pengguna sebelum disimpan ke database
-      bcrypt.hash(password, 10, (err, hashedPassword) => {
-        if (err) {
-          console.error('Error hashing password: ', err);
-          res.status(500).json({ error: 'Terjadi kesalahan saat mendaftar.' });
-        } else {
-          // Simpan data pengguna ke database
-          const insertUserQuery = 'INSERT INTO user (userid, username, userpasswd) VALUES (?, ?, ?)';
-          const values = [userid, username, hashedPassword];
-          db.query(insertUserQuery, values, (err, result) => {
-            if (err) {
-              console.error('Error executing SQL query: ', err);
-              res.status(500).json({ error: 'Terjadi kesalahan saat mendaftar.' });
-            } else {
-              res.status(200).json({ message: 'Registrasi berhasil.', userId: result.insertId });
-            }
-          });
-        }
-      });
-    }
+    // Periksa apakah pengguna sudah terdaftar
+    const checkUserQuery = 'SELECT * FROM user WHERE username = ?';
+    db.query(checkUserQuery, [username], (err, result) => {
+      if (err) {
+        console.error('Error executing SQL query: ', err);
+        res.status(500).json({ error: 'Terjadi kesalahan saat mendaftar.' });
+      } else if (result.length > 0) {
+        res.status(409).json({ error: 'Username sudah digunakan.' });
+      } else {
+        // Generate salt untuk proses hashing
+        bcrypt.genSalt(10, (err, salt) => {
+          if (err) {
+            console.error('Error generating salt: ', err);
+            res.status(500).json({ error: 'Terjadi kesalahan saat mendaftar.' });
+          } else {
+            // Hash password menggunakan salt
+            bcrypt.hash(password, salt, (err, hashedPassword) => {
+              if (err) {
+                console.error('Error hashing password: ', err);
+                res.status(500).json({ error: 'Terjadi kesalahan saat mendaftar.' });
+              } else {
+                // Simpan data pengguna ke database
+                const insertUserQuery = 'INSERT INTO user (userid, username, userpasswd) VALUES (?, ?, ?)';
+                const values = [userid, username, hashedPassword];
+                db.query(insertUserQuery, values, (err, result) => {
+                  if (err) {
+                    console.error('Error executing SQL query: ', err);
+                    res.status(500).json({ error: 'Terjadi kesalahan saat mendaftar.' });
+                  } else {
+                    res.status(200).json({ message: 'Registrasi berhasil.', userId: result.insertId });
+                  }
+                });
+              }
+            });
+          }
+        });
+      }
+    });
   });
-});
 
-app.put('/route/:id', (req, res) => {
-  const id = req.params.id;
-  const { shiptoname, address, remarks, area } = req.body;
-  const sql = 'UPDATE route SET shiptoname = ?, address = ?, remarks = ?, area = ? WHERE id = ?';
-  const values = [shiptoname, address, remarks, area, id];
 
-  db.query(sql, values, (err, result) => {
-    if (err) {
-      console.error('Error executing SQL query: ', err);
-      res.status(500).json(err);
-    } else {
-      res.json(result);
-    }
+  app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    // Ambil data pengguna dari database berdasarkan username
+    const getUserQuery = 'SELECT * FROM user WHERE username = ?';
+    db.query(getUserQuery, [username], (err, result) => {
+      if (err) {
+        console.error('Error executing SQL query: ', err);
+        res.status(500).json({ error: 'Terjadi kesalahan saat login.' });
+      } else if (result.length === 0) {
+        res.status(401).json({ error: 'Username atau password salah.' });
+      } else {
+        // Bandingkan password yang diinput dengan password di database menggunakan bcrypt
+        bcrypt.compare(password, result[0].userpasswd, (err, isMatch) => {
+          if (err) {
+            console.error('Error comparing passwords: ', err);
+            res.status(500).json({ error: 'Terjadi kesalahan saat login.' });
+          } else if (isMatch) {
+            res.status(200).json({ message: 'Login berhasil.', userId: result[0].userid });
+          } else {
+            res.status(401).json({ error: 'Username atau password salah.' });
+          }
+        });
+      }
+    });
   });
-});
 
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
+  app.delete('/route/:id', (req, res) => {
+    const id = req.params.id;
+    const sql = 'DELETE FROM route WHERE id = ?';
 
-  // Ambil data pengguna dari database berdasarkan username
-  const getUserQuery = 'SELECT * FROM user WHERE username = ?';
-  db.query(getUserQuery, [username], (err, result) => {
-    if (err) {
-      console.error('Error executing SQL query: ', err);
-      res.status(500).json({ error: 'Terjadi kesalahan saat login.' });
-    } else if (result.length === 0) {
-      res.status(401).json({ error: 'Username atau password salah.' });
-    } else {
-      // Bandingkan password yang diinput dengan password di database menggunakan bcrypt
-      bcrypt.compare(password, result[0].userpasswd, (err, isMatch) => {
-        if (err) {
-          console.error('Error comparing passwords: ', err);
-          res.status(500).json({ error: 'Terjadi kesalahan saat login.' });
-        } else if (isMatch) {
-          res.status(200).json({ message: 'Login berhasil.', userId: result[0].userid });
-        } else {
-          res.status(401).json({ error: 'Username atau password salah.' });
-        }
-      });
-    }
+    db.query(sql, id, (err, result) => {
+      if (err) {
+        console.error('Error executing SQL query: ', err);
+        res.status(500).json(err);
+      } else {
+        res.json(result);
+      }
+    });
   });
-});
 
-app.delete('/route/:id', (req, res) => {
-  const id = req.params.id;
-  const sql = 'DELETE FROM route WHERE id = ?';
+  app.get('/user', (req, res) => {
+    const userId = req.query.userId; // Mendapatkan userId dari parameter query
 
-  db.query(sql, id, (err, result) => {
-    if (err) {
-      console.error('Error executing SQL query: ', err);
-      res.status(500).json(err);
-    } else {
-      res.json(result);
-    }
+    // Ambil data pengguna dari database berdasarkan userId
+    const getUserQuery = 'SELECT * FROM user WHERE userid = ?';
+    db.query(getUserQuery, [userId], (err, result) => {
+      if (err) {
+        console.error('Error executing SQL query: ', err);
+        res.status(500).json({ error: 'Terjadi kesalahan saat mengambil data pengguna.' });
+      } else if (result.length === 0) {
+        res.status(404).json({ error: 'Pengguna tidak ditemukan.' });
+      } else {
+        // Menghapus password dari objek hasil query
+        const user = { ...result[0] };
+        delete user.userpasswd;
+
+        res.status(200).json(user);
+      }
+    });
   });
-});
 
-app.listen(port, () => {
-  console.log('Listening on port ' + port);
-});
+
+  app.listen(port, () => {
+    console.log('Listening on port ' + port);
+  });
